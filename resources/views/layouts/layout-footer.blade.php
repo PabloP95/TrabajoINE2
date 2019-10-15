870e0856a317ad03173b4eb0ef00e37914076a67
Pablo Piedad Garrido <45001101+PabloP95@users.noreply.github.com>
2019-10-15 10:26:25 +0200

    Subida archivos
    Hemos cargado los layouts para el header y el footer

---
+++ resources/views/layouts/layout-footer.blade.php
@@ -0,0 +1,48 @@
+<hr class="clearfix v='100' b-md-noun">
+<div class="row" style="background: pink">
+    <div class="col-md-3 mx-auto clearfix" style="padding-left: 3.5em;">
+        <br>
+        <b>Acerca de</b>
+        <ul class="list-unstyled ulEspaciado">
+            <li><a href="#" class="small aFooter">Quiénes somos</a></li>
+            <li><a href="#" class="small aFooter">Trabaja con nosotros</a></li>
+            <li><a href="#" class="small aFooter">Aviso legal</a></li>
+        </ul>
+    </div>
+    <div class="col-md-3 mx-auto clearfix">
+        <br>
+        <b>Navegación</b>
+        <ul class="list-unstyled ulEspaciado">
+            <li><a href="#" class="small aFooter">Idioma / languages</a></li>
+            <li><a href="#" class="small aFooter">Mapa del sitio</a></li>
+        </ul>
+    </div>
+    <div class="col-md-3 mx-auto clearfix">
+        <br>
+        <b>Ayuda</b>
+        <ul class="list-unstyled ulEspaciado">
+            <li><a href="#" class="small aFooter">Gestionar compras</a></li>
+            <li><a href="#" class="small aFooter">Tarifas y políticas de envío</a>
+                <a href='#'>
+                    <img src="ico/gorjeo.png" width = '16' height = '16' style = "position: absolute; right: -10px; top: 83px"> 
+                </a>
+            </li>
+            <li><a href="#" class="small aFooter">Devolver o reemplazar productos</a></li>
+            <li><a href="#" class="small aFooter">Contacto para ayuda</a></li>
+            <li><a href="#" class="small aFooter">IVA sobre los bienes</a></li>
+            <li><a href="#" class="small aFooter">Métodos de pago</a></li>
+        </ul>
+        <br>
+    </div>
+    <div class="col-md-3 mx-auto clearfix">
+        <br>
+        <b>Contacto</b>
+        <ul class="list-unstyled ulEspaciado">
+            <li><a href="#" class="small aFooter">Atención al cliente</a></li>
+            <li><a href="#" class="small aFooter">Twitter</a></li>
+            <li><a href="#" class="small aFooter">Facebook</a></li>
+            <li><a href="#" class="small aFooter">Youtube</a></li>
+            <li><a href="#" class="small aFooter">Instagram</a></li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/layouts/layout-header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-sm" style="background:pink">
+    <a class="navbar-brand" href="#" style = "color:#34B343"><h2>Vidya gaems</h2></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="buscar" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
